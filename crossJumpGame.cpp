e640f2eace085607f98f097fb5a8d4269b54b54d
Sukhbir Singh <sukhbir947@gmail.com>
2015-11-06 23:23:07 +0530

    Fixed previous bug

--- crossJumpGame.cpp
+++ crossJumpGame.cpp
@@ -15,8 +15,10 @@ int attach=0;	// wheather to attach user with box or not
 float dx=0.0; // distance between player and inside box
 
 float xtest[8]={0,850,20,0,13,857,100,800};
+//float xtest[8]={200,200,200,200,200,200,200,200s};
 float ytest[8]={121,171,221,271,327,377,427,477};
 float speedtest[8]={0.4,-0.3,0.45,0,0.42,-0.52,0.47,-0.56};
+//float speedtest[8]={0,0,0,0,0,0,0,0};
 float width[8]={120,130,140,windowWidth,150,127,117,110};
 int attachedbox=-1;
 
@@ -31,6 +33,7 @@ void init(void)
 
 void display(void)
 {
+	
 	glClear(GL_COLOR_BUFFER_BIT);	// clear color buffer
 	
 	
@@ -51,7 +54,7 @@ void display(void)
 	glBegin(GL_QUADS);
 	{
 
-	for(int j=0;j<8;j++)
+	for(int j=0;j<8;j++)	//All Boxes
 		{
 			glVertex3f(xtest[j],ytest[j]-25,0);
 			glVertex3f(xtest[j]+width[j],ytest[j]-25,0);
@@ -88,7 +91,7 @@ void display(void)
 	int xtemp=x;
 	if(attach==1)
 	{
-		x=xtest[0]+dx;
+		x=xtest[attachedbox]+dx;
 		//cout<<"xtest "<<xtest<<" x"<<x<<"\n";
 	}
 
@@ -106,20 +109,22 @@ void display(void)
 	//x=xtemp;
 
 	glFlush();					// forces drawing to complete
+	
 }
 
 void reshape(int w, int h)
-{
+{	
 	windowHeight=h;
 	windowWidth=w;
 glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 glMatrixMode(GL_PROJECTION);
 glLoadIdentity();
 glOrtho(0.0,(GLdouble)w,0.0,(GLdouble)h,0.0,1.0);
+
 }
 
 void againDisplay()
-{
+{	
 		//	xtest=xtest+speedtest;
 		for(int j=0;j<8;j++)
 		{
@@ -151,15 +156,13 @@ void againDisplay()
 		}
 
 
-		
 	glutPostRedisplay();
-}
+	 }
 
 //For keyboard, p for pause 
 // 112 for 'p' and 32 for 'space'
 void key(unsigned char key,int xm,int ym)
-{
-
+{	
 	//cout<<"KEY.... ";
 
 	if(key==97)
@@ -179,44 +182,27 @@ void key(unsigned char key,int xm,int ym)
 
 int flag=0;
 	
-	/*if(y>=800)
-	{
-		play=0;
-		glutIdleFunc(NULL);
-		attach=1;
-		cout<<"You are on Winner.\n";
-		flag=1;
-			
-	}*/
-
-	//  to make players box remain on the rail boxes  
-/*	if ((y>=96 && y<=146)&&(xtest[0]<=x && xtest[0]+width[0]>=x))		// that's the best place to make this...
-		{
-			dx=x-xtest[0];
-			cout<<"attach=1\n";
-			attach=1;
-
-		}else {attach=0; cout<<"attach=0\n";}
-		*/
-	
-
 	for(int j=0;j<8;j++)
 	{	// that's the best place to make this...
+		
+	
 		if ((y>=ytest[j]-25 && y<=ytest[j]+25 )&&( xtest[j]<=x && xtest[j]+width[j]>=x))		
 		{
-			dx=x-xtest[0];
+			dx=x-xtest[j];
+			if(dx<0) dx=-dx;
 			cout<<"attach=1\n";
 			attach=1;
-			cout<<"You are on "<<j<<" - box.\n";
 			flag=1;
 			attachedbox=j;
+			cout<<"You are on "<<j<<" - box. and attachedbox is "<<attachedbox<<"\n";
+			
 			break;
 
-		}
+		}else {cout<<"y "<<y<<" ytest "<<ytest[j]<<" j "<<j<<"\n";}
 
 	}
 
-	if(flag==0) {attach=0; cout<<"attach=0\n";} 
+	if(flag==0) {attach=0; cout<<"x: "<<x<<" y: "<<y<<" and Attach1=0\n";} 
 
 	/*
 	float xtest[7]={0,850,20,13,857,100,800};
